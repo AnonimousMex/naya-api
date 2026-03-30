7eee779d69df17e48432a631f7634211a2ec3ea6
AnonimousMex <drc20101870@gmail.com>
2026-03-30 16:09:11 -0600

    feat(N0104): Test results has been completed

--- app/api/test/test_router.py
+++ app/api/test/test_router.py
@@ -1,32 +1,39 @@
-
 from typing import List
 from fastapi import APIRouter, HTTPException, Header
 from fastapi.encoders import jsonable_encoder
 
 from app.api.test.test_controller import TestController
-from app.api.test.test_schema import AnswersRequest, AnswersResponse, EmotionPercentageResponse, ListTestRequest , PostAnswerRequest, TestDetailsReponse, TestDetailsRequest, TestStoriesResponse, TestsResponse
+from app.api.test.test_schema import (
+    AnswersRequest,
+    AnswersResponse,
+    EmotionPercentageResponse,
+    ListTestRequest,
+    PostAnswerRequest,
+    TestDetailsReponse,
+    TestDetailsRequest,
+    TestStoriesResponse,
+    TestsResponse,
+)
 from app.core.database import SessionDep
 from app.core.http_response import NayaHttpResponse, NayaResponseModel
 
 test_router = APIRouter(prefix="/test")
 
-@test_router.get(
-        "/init-test", 
-        response_model=NayaResponseModel[TestStoriesResponse]
-)
+
+@test_router.get("/init-test", response_model=NayaResponseModel[TestStoriesResponse])
 async def get_situtations(
-    session: SessionDep, 
-    token: str = Header(..., alias="Authorization")
+    session: SessionDep, token: str = Header(..., alias="Authorization")
 ):
     try:
         test_controller = TestController(session=session)
         data = await test_controller.get_stories(token=token)
-        return NayaHttpResponse.ok(data=jsonable_encoder(data)) 
+        return NayaHttpResponse.ok(data=jsonable_encoder(data))
     except HTTPException as e:
         raise e
     except Exception as e:
         raise e
-    
+
+
 @test_router.post("/send-answer")
 async def post_answer(session: SessionDep, request: PostAnswerRequest):
     try:
@@ -36,14 +43,14 @@ async def post_answer(session: SessionDep, request: PostAnswerRequest):
         raise e
     except Exception as e:
         raise e
-    
+
+
 @test_router.post(
-        "/list-answers",
-        response_model=NayaResponseModel[List[AnswersResponse]]
+    "/list-answers", response_model=NayaResponseModel[List[AnswersResponse]]
 )
-async def list_answers(session: SessionDep, request: AnswersRequest ):
-    try: 
-        test_controller = TestController(session=session) 
+async def list_answers(session: SessionDep, request: AnswersRequest):
+    try:
+        test_controller = TestController(session=session)
         data = await test_controller.list_answers(request=request)
         return NayaHttpResponse.ok(data=jsonable_encoder(data))
     except HTTPException as e:
@@ -51,10 +58,9 @@ async def list_answers(session: SessionDep, request: AnswersRequest ):
     except Exception as e:
         raise e
 
-@test_router.post(
-        "/test-details",
-        response_model=NayaResponseModel[TestDetailsReponse]
-)
+
+# test_router.post("/percentage-answers") - is imporant to calculate the percentage of each emotion in the answers of the test
+@test_router.post("/test-details", response_model=NayaResponseModel[TestDetailsReponse])
 async def test_detail(session: SessionDep, request: TestDetailsRequest):
     try:
         test_controller = TestController(session=session)
@@ -64,10 +70,11 @@ async def test_detail(session: SessionDep, request: TestDetailsRequest):
         raise e
     except Exception as e:
         raise e
-    
+
+
 @test_router.post(
-        "/percentage-answers",
-        response_model=NayaResponseModel[List[EmotionPercentageResponse]]
+    "/percentage-answers",
+    response_model=NayaResponseModel[List[EmotionPercentageResponse]],
 )
 async def answers_details(session: SessionDep, request: AnswersRequest):
     try:
@@ -78,13 +85,10 @@ async def answers_details(session: SessionDep, request: AnswersRequest):
         raise e
     except Exception as e:
         raise e
-    
-@test_router.post(
-        "/list-test",
-        response_model=NayaResponseModel[List[TestsResponse]]
-)
-async def list_test(
-    session: SessionDep, request: ListTestRequest):
+
+
+@test_router.post("/list-test", response_model=NayaResponseModel[List[TestsResponse]])
+async def list_test(session: SessionDep, request: ListTestRequest):
     try:
         test_controller = TestController(session=session)
         tests = await test_controller.list_test(request)
