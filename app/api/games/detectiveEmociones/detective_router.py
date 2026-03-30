b295de1e297b3deb70d2cc1e0692597dc9dfc37d
AnonimousMex <drc20101870@gmail.com>
2026-03-30 16:00:29 -0600

    feat(N0103): Api bug has been fixed

--- app/api/games/detectiveEmociones/detective_router.py
+++ app/api/games/detectiveEmociones/detective_router.py
@@ -2,16 +2,18 @@ from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
 
 from app.api.games.detectiveEmociones.detective_controller import DetectiveController
-from app.api.games.detectiveEmociones.detective_schema import DetectiveSituationsResponse
+from app.api.games.detectiveEmociones.detective_schema import (
+    DetectiveSituationsResponse,
+)
 from app.core.database import SessionDep
 from app.core.http_response import NayaHttpResponse, NayaResponseModel
 
-
 detective_router = APIRouter(prefix="/detective")
 
+
+# Responses for the game of detective emociones, this game consists of showing a situation and the user has to choose the emotion that corresponds to that situation, the emotions are: happy, sad, angry, scared, surprised and neutral. The situations are stored in the database and are retrieved by the controller.
 @detective_router.get(
-    "/game",
-    response_model=NayaResponseModel[list[DetectiveSituationsResponse]]
+    "/game", response_model=NayaResponseModel[list[DetectiveSituationsResponse]]
 )
 async def get_situations(session: SessionDep):
     try:
